aaeddff45725a88090ad1a128321871a5814d858
sushmitha <kotyadasushmithad@gmail.com>
2018-10-28 19:09:28 -0500

    made chages

--- public/js/calculation.js
+++ public/js/calculation.js
@@ -1,8 +1,8 @@
 var n1 = parseFloat(document.getElementById('n1').value);
  var n2 = parseFloat(document.getElementById('n2').value);
  var oper = document.getElementById('operators').value;
-var a = calc(n1,n2,oper)
-document.getElementById('result').value = a
+var a = calc(n1,n2,oper);
+document.getElementById('result').value = a;
 function calc(n1,n2,oper)
             {
 //                 var n1 = parseFloat(document.getElementById('n1').value);
