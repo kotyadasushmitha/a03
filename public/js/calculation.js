04f0ce26f3781d4e03da8235071911d3219d6874
sushmitha <kotyadasushmithad@gmail.com>
2018-10-28 19:02:15 -0500

    hhh

--- public/js/calculation.js
+++ public/js/calculation.js
@@ -1,31 +1,41 @@
-function calc()
+var n1 = parseFloat(document.getElementById('n1').value);
+ var n2 = parseFloat(document.getElementById('n2').value);
+ var oper = document.getElementById('operators').value;
+var a = calc(n1,n2,oper)
+document.getElementById('result').value = a
+function calc(n1,n2,oper)
             {
-                var n1 = parseFloat(document.getElementById('n1').value);
-                var n2 = parseFloat(document.getElementById('n2').value);
+//                 var n1 = parseFloat(document.getElementById('n1').value);
+//  var n2 = parseFloat(document.getElementById('n2').value);
                 
-                var oper = document.getElementById('operators').value;
+                //var oper = document.getElementById('operators').value;
                 
                 if(oper === '+')
                 {
-                    document.getElementById('result').value = n1+n2;
+                    //document.getElementById('result').value = n1+n2;
+                    return n1+n2;
                 }
                 
                 if(oper === '-')
                 {
-                    document.getElementById('result').value = n1-n2;
+                    //document.getElementById('result').value = n1-n2;
+                    return n1-n2;
                 }
                 
                 if(oper === '/')
                 {
-                    document.getElementById('result').value = n1/n2;
+                    //document.getElementById('result').value = n1/n2;
+                    return n1/n2;
                 }
                 
                 if(oper === 'X')
                 {
-                    document.getElementById('result').value = n1*n2;
+                    //document.getElementById('result').value = n1*n2;
+                    return n1*n2;
                 }
 				if(oper === '%')
                 {
-                    document.getElementById('result').value = n1%n2;
+                    //document.getElementById('result').value = n1%n2;
+                    return n1%n2;
                 }
             }
